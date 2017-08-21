37465f3a58e06d9c1f559441654f89f758fb4a85
Oscar Borrego <oscar.borrego@sensus.com>
2017-08-21 08:39:21 -0400

    added afplay.bat prototype

---
+++ sbin/afplay.bat
@@ -0,0 +1,11 @@
+@echo off
+echo %1
+set "file=%1"
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
+start /min sound.vbs
